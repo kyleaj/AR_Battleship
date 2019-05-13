e4ae89f2368d252fcd4a6d8a385a2de10d8264c3
PaliC <sahancpal@gmail.com>
2019-05-13 19:23:25 -0400

    placement activity

--- app/src/main/java/com/cos426/ar_battleship/Board.java
+++ app/src/main/java/com/cos426/ar_battleship/Board.java
@@ -8,16 +8,17 @@ public class Board {
     Ship[][] shipLocations;
     int livingShips;
     ArrayList<Ship> ships;
+    private final int BOARD_SIZE = GameInfo.BOARD_SIZE;
     public Board(){
-        shots_fired = new boolean[9][9];
-        hits = new boolean[9][9];
-        shipLocations = new Ship[9][9];
+        shots_fired = new boolean[BOARD_SIZE][BOARD_SIZE];
+        hits = new boolean[BOARD_SIZE][BOARD_SIZE];
+        shipLocations = new Ship[BOARD_SIZE][BOARD_SIZE];
         livingShips = 0;
         ships = new ArrayList<Ship>();
     }
 
     public boolean shoot(int x, int y){
-        if(x >= 9 || x < 0) return false;
+        if(x >= BOARD_SIZE || x < 0) return false;
         shots_fired[x][y] = true;
         if(shipLocations[x][y] != null){
             Ship shotShip = shipLocations[x][y];
@@ -32,7 +33,36 @@ public class Board {
 //    Not exactly sure how to represent the list of
 //    coordinates for the ship, But is supposed to add
 //    the ship to the board and return false if it is not possible to
-    public boolean addShip(){
-        return false;
+    public boolean addShip(int x, int y, int size, boolean xAxis){
+        Ship newShip = new Ship(size,x,y,xAxis);
+        if(xAxis){
+            for(int i = 0; i<size; i++){
+                if(shipLocations[i][y] != null) return false;
+            }
+        }else{
+            for(int i = 0; i<size; i++){
+                if(shipLocations[x][i] != null) return false;
+            }
+        }
+
+        if(xAxis){
+            for(int i = 0; i<size; i++){
+               shipLocations[i][y] = newShip;
+            }
+        }else{
+            for(int i = 0; i<size; i++){
+                shipLocations[x][i] = newShip;
+            }
+        }
+        livingShips++;
+        ships.add(newShip);
+        return true;
+    }
+    public void resetBoard(){
+        shots_fired = new boolean[BOARD_SIZE][BOARD_SIZE];
+        hits = new boolean[BOARD_SIZE][BOARD_SIZE];
+        shipLocations = new Ship[BOARD_SIZE][BOARD_SIZE];
+        livingShips = 0;
+        ships = new ArrayList<Ship>();
     }
 }
